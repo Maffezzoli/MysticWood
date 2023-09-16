7fb0563b554697570473814b2f054d443c736557
Daniel <maffezzolidaniel78@gmail.com>
2023-09-16 14:42:11 -0300

    Aprimorado para godot 4.0

--- Scripts/player_personagem.gd
+++ Scripts/player_personagem.gd
@@ -1,31 +1,66 @@
 extends CharacterBody2D
-@onready var animation: AnimationPlayer = get_node("AnimationPlayer")
-@onready var sprite: Sprite2D = get_node("Sprite2D")
 
-@export var speed: int = 0
+var _state_machine
+var is_attacking: bool = false
+var mov_lock: bool = true
+@export_category("Variables")
+@export var speed: float = 0
+@export var friction: float = 0.2
+@export var aceceleration: float = 0.2
+
+@export_category("Objects")
+@export var attack_timer: Timer = null
+@export var animation_tree: AnimationTree = null
+
+func _ready() -> void:
+	_state_machine = animation_tree["parameters/playback"]
+
 func _physics_process(_delta: float) -> void:
 	move()
-	verify_direction()
+	attack()
 	animate()
-func move() -> void:
-	var direction_vetor: Vector2 = Vector2(
-		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
-		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
-		).normalized()
-		
-	velocity = direction_vetor * speed
 	move_and_slide()
-
-func animate() -> void:
-	if velocity != Vector2.ZERO:
-		animation.play("Run")
-	else:
-		animation.play("Idle")
-
-func verify_direction() -> void: 
-	if velocity.x > 0:
-		sprite.flip_h = false
-	elif velocity.x < 0:
-		sprite.flip_h = true
-
 	
+func move() -> void:
+	var direction_vector: Vector2 = Vector2(0, 0)
+
+	if Input.is_action_pressed("move_right"):
+		direction_vector.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction_vector.x -= 1
+	if Input.is_action_pressed("move_down"):
+		direction_vector.y += 1
+	if Input.is_action_pressed("move_up"):
+		direction_vector.y -= 1
+
+	if direction_vector != Vector2.ZERO:
+		animation_tree["parameters/run/blend_position"] = direction_vector
+		animation_tree["parameters/idle/blend_position"] = direction_vector
+		animation_tree["parameters/attack/blend_position"] = direction_vector
+
+		velocity = direction_vector.normalized() * speed
+	else:
+		velocity = Vector2.ZERO
+
+func attack() -> void:
+	if Input.is_action_just_pressed("attack") and not is_attacking:
+		set_physics_process(false)
+		attack_timer.start()
+		is_attacking = true
+	
+func animate() ->void: 
+	if is_attacking:
+		_state_machine.travel("attack") 
+		return
+		
+	if velocity.length() > 10:
+		_state_machine.travel("run")
+		return
+	_state_machine.travel("idle")
+	
+		
+
+
+func _on_attack_timer_timeout():
+	set_physics_process(true)
+	is_attacking = false
